5e0b9c5a30e44d80abb4974ddfa648ed7abb8686
Student User <student@workstation.lab.example.com>
2020-02-18 22:30:51 +0000

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
-print "A change is in the air!\n";
-=======
-print "hiee\n";
->>>>>>> 28fcbbb94ef31f60c9bcc68735505df871526f1e
+print "A change is in the air! - mcf\n";
 ?>
